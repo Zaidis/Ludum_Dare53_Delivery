55f2f4e360e34c618ba8a483acff31c444392d07
Adam_Weber <chewybinx@gmail.com>
2023-04-30 17:09:39 -0500

    initial game manager

---
+++ Smuggle/Assets/Scripts/GameManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    
+    //needs to hold onto time, and whatnot
+
+}
